453a97933a9cff85b86601058289b042a5610451
FMXExpress <fmx@peacekeeper.com>
2020-07-24 16:57:52 -0700

    Revert "Added 10.4"
    This reverts commit c68bb30e0458107094904253a5bb182e3057b077.

--- 10.4/Solutions/Example-Child-Image/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == '' ] || [ "$2" == '' ]; then
-    echo "RAD Server Docker pa-radserver-child Build Script";
-    echo "Required arguments: RAD Server database (InterBase) host and port";
-    echo "ex: build.sh example.com 3050";
-else
-    docker build --build-arg password=securepass --build-arg dbhost=$1 --build-arg dbport=$2 -t pa-radserver-child
-fi
--- 10.4/Solutions/Example-Child-Image/pull.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
- docker pull radstudio/pa-radserver
