08562a55a1774ea6786051f8e391af9f41179cd6
Serhii Kheilyk <1171495+lynxnake@users.noreply.github.com>
2022-12-21 07:48:55 +0200

    tag built image as pa-radserver

--- 11.2/build.sh
+++ 11.2/build.sh
@@ -5,5 +5,5 @@ if [ "$1" == '' ] || [ "$2" == '' ]; then
     echo "Required arguments: RAD Server database (InterBase) host and port";
     echo "ex: build.sh example.com 3050";
 else
-    docker build --build-arg password=securepass --build-arg dbhost=$1 --build-arg dbport=$2 .
+    docker build --build-arg password=securepass --build-arg dbhost=$1 --build-arg dbport=$2 --tag pa-radserver .
 fi
