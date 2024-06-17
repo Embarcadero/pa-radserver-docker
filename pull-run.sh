10cbd21ae787ce2bb04fbb714bc2b177273fcf49
lmbelo <lucas.belo@live.com>
2024-06-17 18:29:10 -0300

    Archiving 12.1 Athens

--- pull-run.sh
+++ pull-run.sh
@@ -5,7 +5,7 @@ if [ "$1" = '' ] || [ "$2" = '' ]; then
     echo "Required arguments: RAD Server database (InterBase) host and port";
     echo "ex: pull-run.sh example.com 3050";
 else
-    docker pull radstudio/pa-radserver:latest
+    docker pull radstudio/pa-radserver:12.1
 
     bash ./run.sh $1 $2
 fi
--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ else
         -it --mount source=ems,target=/etc/ems \
 	-e DB_PORT=$2 -e DB_HOST=$1 \
         -e PA_SERVER_PASSWORD=securepass -p 80:80 -p 64211:64211 -p 8082:8082 \
-	radstudio/pa-radserver:latest
+	radstudio/pa-radserver:12.1
 fi
